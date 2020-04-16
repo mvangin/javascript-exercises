87cb2010c883166e2e54ffd551f6409400e59acc
mvangin <43646116+mvangin@users.noreply.github.com>
2020-04-15 23:17:37 -0700

    complete reverseString Assignment

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
 
+    let reversedArray = stringToReverse.split("");
+    reversedArray.reverse();
+    reversedString = reversedArray.join("");
+    return reversedString;
+
+
+    
+  //  for (let i=stringToReverse.length-1; i>=0; i--) {
+    //    reversedString += stringToReverse[i];
+    //}
+    //return reversedString;
 }
 
 module.exports = reverseString
+
+
