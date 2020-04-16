cbe3380b25e7d44b7289ad82ee2bce020a73f483
mvangin <43646116+mvangin@users.noreply.github.com>
2020-04-15 21:49:09 -0700

    complete repeatString assignment

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(wordInput, numberOfRepeats) {
+    let repeatWord = "";
 
+    for  (let i=0; i < numberOfRepeats; i++) {
+        repeatWord += wordInput;
+    }
+    if (numberOfRepeats < 0) {
+        return "ERROR";
+        } else {
+    return repeatWord;
+    }
 }
 
 module.exports = repeatString
