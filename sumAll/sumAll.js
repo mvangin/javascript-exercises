3206d0544f2f2ef6f490697116742a78a90f9bc1
mvangin <43646116+mvangin@users.noreply.github.com>
2020-04-16 18:36:22 -0700

    complete sumAll assignment

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumTotal = 0;
 
+    if (num1 < 0 || num2 < 0 || typeof(num1) != "number" || typeof(num2) != "number") {
+        return "ERROR"
+    }
+    
+    start = Math.min(num1, num2);
+    end = Math.max(num1,num2);
+
+
+    for (let i=start; i<=end; i++) {
+        sumTotal += i;
+    }
+    return sumTotal;
 }
 
 module.exports = sumAll
