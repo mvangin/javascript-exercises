734051fcc270850705ac51639277a19d1683ee30
mvangin <43646116+mvangin@users.noreply.github.com>
2020-04-16 19:15:21 -0700

    complete tempConversion assignment

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(Fahrenheit) {
+let Celsius = (Fahrenheit -32) * 5/9
+return Math.round(Celsius *10) / 10;
 }
 
-const ctof = function() {
+const ctof = function(Celsius) {
+let Fahrenheit = Celsius * 9/5 + 32;
+return Math.round(Fahrenheit * 10) /10;
 
 }
 
